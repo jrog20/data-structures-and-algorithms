edbacf06e23b72ca01dd6b8bae318e253e3ec486
jrog20 <jaimerogstad@gmail.com>
2021-10-29 18:58:27 -0400

    Add function

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,9 +1,15 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const uniques = new Set();
+  for(let i = 0; i < arr.length; i++) {
+    if(uniques.has(arr[i])) {
+      return arr[i];
+    }
+    uniques.add(arr[i]);
+  }
+  return -1
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
@@ -15,5 +21,24 @@ if (require.main === module) {
 
 module.exports = findFirstDuplicate;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// Given an Array, find the first duplicate value that occurs. 
+// If there are no duplicates, return -1.
+
+// # pseudocode
+// # ******************************************
+// # initiate a new Set
+// # iterate over the array 
+//   # return the element if the element exists in the set
+//   # else add the element to the set
+// # return -1 if no duplicate is found
+// # ******************************************
+
+// # written explanation of solution
+// # ******************************************
+// # A Set is a data structure the contains only unique values
+// # if I iterate over the array and check if each value is in the Set,
+// # I can return the first value that exists in the set because that is 
+// # the first duplicate value.
+// # If I get to the end of the array and have not found a duplicate,
+// # return -1.
+// # ******************************************
--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/ruby/find_first_duplicate.rb
@@ -26,17 +26,21 @@ end
 
 # Given an Array, find the first duplicate value that occurs. If there are no duplicates, return -1.
 
-# Please add your pseudocode to this file
+# pseudocode
+# ******************************************
 # initiate a new Set
 # iterate over the array 
   # return the element if the element exists in the set
   # else add the element to the set
 # return -1 if no duplicate is found
+# ******************************************
 
-# And a written explanation of your solution
+# written explanation of solution
+# ******************************************
 # A Set is a data structure the contains only unique values
 # if I iterate over the array and check if each value is in the Set,
 # I can return the first value that exists in the set because that is 
 # the first duplicate value.
 # If I get to the end of the array and have not found a duplicate,
 # return -1.
+# ******************************************
