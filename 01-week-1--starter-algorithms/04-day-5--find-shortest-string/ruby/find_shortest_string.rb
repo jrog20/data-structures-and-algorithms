e8dd7286fcc3d6fc4e8608481670f1e77c0e1ede
jrog20 <jaimerogstad@gmail.com>
2021-11-02 21:08:57 -0400

    Add method

--- 01-week-1--starter-algorithms/04-day-5--find-shortest-string/ruby/find_shortest_string.rb
+++ 01-week-1--starter-algorithms/04-day-5--find-shortest-string/ruby/find_shortest_string.rb
@@ -1,5 +1,15 @@
 def find_shortest_string(arr)
-  # type your code in here
+  # shortest = arr[0]
+  # arr.each do |string|
+  #   if string.length < shortest.length
+  #     shortest = string
+  #   end
+  # end
+  # return shortest
+
+  arr.reduce do |shortest, string| 
+    string.length < shortest.length ? string : shortest
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -16,10 +26,22 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'lily'"
   puts "=>", find_shortest_string(['flower', 'juniper', 'lily', 'dadelion'])
 
-  # Don't forget to add your own!
-
-  # BENCHMARK HERE
 end
 
-# Please add your pseudocode to this file
-# And a written explanation of your solution
+# The problem:
+# Given an Array of strings, return the shortest string. 
+# If there is more than one string of that length, 
+# return the string that comes first in the list. 
+# The Array will have a minimum length of 1.
+
+# pseudocode:
+# shortest = first string
+# iterate over the array of strings
+  # if the current string is < shortest
+  # shortest = current string
+# return shortest
+
+# explanation:
+# If we store the first string in a variable, we can then
+# compare each subsequent string length to the first. If it is shorter,
+# we can replace the variable with the new shortest string.
