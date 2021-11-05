bfe270d8cdec92aa8f5e3b293d45e42575178c67
jrog20 <jaimerogstad@gmail.com>
2021-11-05 19:07:37 -0400

    Add function

--- 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
+++ 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
@@ -1,9 +1,16 @@
 function recursiveSearch(arr, target) {
-  // type your code here
+  // base cases
+  if(arr.length === 0) {
+    return false;
+  }
+  if(arr[0] === target) {
+    return true;
+  }
+
+  return recursiveSearch(arr.slice(1), target);
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: true");
   console.log("=>", recursiveSearch([1, 2, 3], 2));
 
@@ -15,5 +22,26 @@ if (require.main === module) {
 
 module.exports = recursiveSearch;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// Recursive Search
+
+// Given an Array of values, use recursion to find the target value. 
+// Return `true` if found, otherwise `false`.
+
+// ```
+// Input: [1, 2, 3], 2
+// Output: true
+
+// Input: [3, 2, 1], 4
+// Output: false
+// ```
+
+// Iterative solution:
+// function iterativeSearch(arr, target) {
+//   for (const value of arr) {
+//     if (value === target) {
+//       return true;
+//     }
+//   }
+
+//   return false;
+// }
