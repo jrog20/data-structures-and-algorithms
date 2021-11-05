58a4c9d8fe40a39c01e236362a20cfb6ebbd2a54
jrog20 <jaimerogstad@gmail.com>
2021-11-05 19:06:45 -0400

    Add method

--- 02-week-2--recursion/02-day-2--recursive-search/ruby/recursive_search.rb
+++ 02-week-2--recursion/02-day-2--recursive-search/ruby/recursive_search.rb
@@ -1,5 +1,9 @@
 def recursive_search(arr, target)
-  # type your code in here
+  # base cases
+  return false if arr.empty?
+  return true if arr.first == target
+  
+  recursive_search(arr[1..-1], target)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -10,9 +14,26 @@ if __FILE__ == $PROGRAM_NAME
 
   puts "Expecting: false"
   puts "=>", recursive_search([3, 2, 1], 4)
-
-  # Don't forget to add your own!
 end
 
-# Please add your pseudocode to this file
-# And a written explanation of your solution
+# Recursive Search
+
+# Given an Array of values, use recursion to find the target value. 
+# Return `true` if found, otherwise `false`.
+
+# ```
+# Input: [1, 2, 3], 2
+# Output: true
+
+# Input: [3, 2, 1], 4
+# Output: false
+# ```
+
+# Iterative solution:
+# def iterative_search(arr, target)
+#   arr.each do |value|
+#     return true if value == target
+#   end
+
+#   false
+# end
