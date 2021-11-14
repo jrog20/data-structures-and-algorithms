a3d68b75b08387968b73eaa1fca39dd4b87d69e3
jrog20 <jaimerogstad@gmail.com>
2021-11-14 18:45:39 -0500

    Add method

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/benchmark/benchmark.rb
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/benchmark/benchmark.rb
@@ -1,7 +1,19 @@
-# Paste your iterative and recursive solutions in this file
-# And then calculate their average run times to compare them.
+# iterative solution
+def find_shortest_string(arr)
+  arr.reduce do |shortest, string|
+    string.length < shortest.length ? string : shortest
+  end
+end
 
-
+# recursive solution
+def find_shortest_string_recursive(arr)
+  # base case
+  return arr.first if arr.length == 1
+  # recursive call
+  result = find_shortest_string_recursive(arr[1..-1])
+  # return the shortest
+  arr.first.length <= result.length ? arr.first : result
+end
 
 def benchmark
   start_time = Time.now
@@ -12,3 +24,6 @@ def benchmark
 
   (Time.now - start_time) / 1000
 end
+
+puts "Iterative Solution: #{benchmark { find_shortest_string(['lily', 'sunflower', '', 'dahlia', 'lilac']) }}"
+puts "Recursive Solution: #{benchmark { find_shortest_string_recursive(['lily', 'sunflower', '', 'dahlia', 'lilac']) }}"
--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
@@ -1,5 +1,10 @@
 def find_shortest_string_recursive(arr)
-  # type your code in here
+  # base case
+  return arr[0] if arr.length == 1
+  # recursive call
+  result = find_shortest_string_recursive(arr[1..-1])
+  # return the shortest
+  arr[0].length <= result.length ? arr[0] : result
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,9 +20,36 @@ if __FILE__ == $PROGRAM_NAME
 
   puts "Expecting: 'lily'"
   puts "=>", find_shortest_string_recursive(['flower', 'juniper', 'lily', 'dandelion'])
-
-  # Don't forget to add your own!
 end
 
-# Please add your pseudocode to this file
-# And a written explanation of your solution
+# #######################################################
+# The Problem: Find the shortest string
+# Given an Array of strings, return the shortest string. 
+# If there is more than one string of that length, 
+# return the string that comes first in the list. 
+# The Array will have a minimum length of 1.
+# #######################################################
+
+# Explanation of Iterative Solution:
+# I want to iterate over the array of strings, 
+# assign the value of the first string to a variable called shortest.
+# I can then compare the length of that string with the length of the next
+# string in the array. If the next string length is shorter, replace the variable 
+# called shortest with the newly found shortest string. Since we are only replacing
+# the variable if the string is shorter, if we encounter a string of the same length
+# as is currently stored in the variable, it will not be replaced and we will return 
+# the first string of the shortest length.
+
+# Iterative Solution:
+# def find_shortest_string(arr)
+#   arr.reduce do |shortest, string|
+#     string.length < shortest.length ? string : shortest
+#   end
+# end
+
+# Explanation of Recursive Solution:
+# Base case = if array of strings is length of 1, return the first string.
+# Then we need to compare every element in the array to find the shortest.
+# We do this by recursively calling the method with a smaller and smaller array,
+# until we only have one string remaining in the array, at which point the 
+# stack starts to unwind, compares each string, and returns the shortest length.
