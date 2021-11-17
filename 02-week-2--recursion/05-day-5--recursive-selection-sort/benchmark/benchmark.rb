ab8189101d785e3910feb96b53b8879b5decf831
jrog20 <jaimerogstad@gmail.com>
2021-11-16 20:06:37 -0500

    Add method

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/benchmark/benchmark.rb
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/benchmark/benchmark.rb
@@ -1,7 +1,30 @@
-# Paste your iterative and recursive solutions in this file
-# And then calculate their average run times to compare them.
+# recursive solution
+def selection_sort_recursive(arr)
+  return [] if arr.length == 0
 
+  min = arr.min
+  idx = arr.index(min)
+  arr.delete_at(idx)
 
+  sorted = selection_sort_recursive(arr)
+  sorted.unshift(min)
+end
+
+# iterative solution
+def selection_sort(arr)
+  sorted = []
+  # iterate through the entire array to find the minimum element
+  # push the minimum element onto the new sorted array
+  # delete the element at the minimum element index 
+  # repeat until the array is empty
+  until arr.length == 0
+    min = arr.min
+    idx = arr.index(min)
+    sorted << min
+    arr.delete_at(idx)
+  end
+  sorted
+end
 
 def benchmark
   start_time = Time.now
@@ -12,3 +35,6 @@ def benchmark
 
   (Time.now - start_time) / 1000
 end
+
+puts "Iterative Solution: #{benchmark { selection_sort([2, 3, 4, 1, 4, 56, -2, 20]) }}"
+puts "Recursive Solution: #{benchmark { selection_sort_recursive([2, 3, 4, 1, 4, 56, -2, 20]) }}"
--- 02-week-2--recursion/05-day-5--recursive-selection-sort/ruby/selection_sort_recursive.rb
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/ruby/selection_sort_recursive.rb
@@ -1,5 +1,12 @@
 def selection_sort_recursive(arr)
-  # type your code in here
+  return [] if arr.length == 0
+
+  min = arr.min
+  idx = arr.index(min)
+  arr.delete_at(idx)
+
+  sorted = selection_sort_recursive(arr)
+  sorted.unshift(min)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -8,9 +15,33 @@ if __FILE__ == $PROGRAM_NAME
   print selection_sort_recursive([3, -1, 5, 2])
 
   puts
-
-  # Don't forget to add your own!
 end
 
-# Please add your pseudocode to this file
-# And a written explanation of your solution
+# The problem: Selection Sort
+# Sort an Array of numbers using selection sort. 
+# The selection sort algorithm sorts an array by 
+# repeatedly finding the minimum element (lowest value) 
+# in the input Array, and then putting it at the correct 
+# location in the sorted Array.
+
+# explanation
+# base case: array == 0
+# call recursive sort function on array, pushing the
+# result of min each time on the front of the array, since
+# the recursive call will unwind at the last call which will 
+# be the largest value in the original array
+
+# def selection_sort(arr)
+#   sorted = []
+#   # iterate through the entire array to find the minimum element
+#   # push the minimum element onto the new sorted array
+#   # delete the element at the minimum element index 
+#   # repeat until the array is empty
+#   until arr.length == 0
+#     min = arr.min
+#     idx = arr.index(min)
+#     sorted << min
+#     arr.delete_at(idx)
+#   end
+#   sorted
+# end
