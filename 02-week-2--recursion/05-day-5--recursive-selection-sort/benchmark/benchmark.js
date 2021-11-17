ef9f465eda6fada9c9a82bc9fa6bf18ee1fd9344
jrog20 <jaimerogstad@gmail.com>
2021-11-17 18:56:15 -0500

    Add function

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/benchmark/benchmark.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/benchmark/benchmark.js
@@ -1,10 +1,28 @@
-// Paste your iterative and recursive solutions in this file
-// And then calculate their average run times to compare them.
+// iterative solution
+function selectionSort(arr) {
+  const sorted = [];
+  while (arr.length > 0) {
+    const min = Math.min(...arr);
+    const idx = arr.indexOf(min);
 
+    sorted.push(min);
+    arr.splice(idx, 1);
+  }
+  return sorted;
+}
 
-
-
-
+// recursive solution
+function selectionSortRecursive(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+  const min = Math.min(...arr);
+  const idx = arr.indexOf(min);
+  arr.splice(idx, 1);
+  const result = selectionSortRecursive(arr);
+  result.unshift(min);
+  return result;
+}
 
 function benchmark(callback) {
   const startTime = Date.now();
@@ -15,3 +33,6 @@ function benchmark(callback) {
 
   return (Date.now() - startTime) / 1000;
 }
+
+console.log('Iterative Solution:', benchmark(() => selectionSort([2, 3, 4, 1, 4, 56, -2, 20])));
+console.log('Recursive Solution:', benchmark(() => selectionSortRecursive([2, 3, 4, 1, 4, 56, -2, 20])));
--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,9 +1,16 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+  if (arr.length === 0) {
+    return [];
+  }
+  const min = Math.min(...arr);
+  const idx = arr.indexOf(min);
+  arr.splice(idx, 1);
+  const result = selectionSortRecursive(arr);
+  result.unshift(min);
+  return result;
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
@@ -12,5 +19,33 @@ if (require.main === module) {
 
 module.exports = selectionSortRecursive;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// The problem: Selection Sort
+// Sort an Array of numbers using selection sort. 
+// The selection sort algorithm sorts an array by 
+// repeatedly finding the minimum element (lowest value) 
+// in the input Array, and then putting it at the correct 
+// location in the sorted Array.
+
+// explanation
+// base case: array == 0
+// call recursive sort function on array, pushing the
+// result of min each time on the front of the array, since
+// the recursive call will unwind at the last call which will 
+// be the largest value in the original array
+
+// iterative solution:
+// function selectionSort(arr) {
+//   const sorted = [];
+//   while (arr.length > 0) {
+//     const min = Math.min(...arr);
+//     const idx = arr.indexOf(min);
+
+//     sorted.push(min);
+//     arr.splice(idx, 1);
+//   }
+//   return sorted;
+// }
+
+// explanation of recursive solution:
+// 
+
