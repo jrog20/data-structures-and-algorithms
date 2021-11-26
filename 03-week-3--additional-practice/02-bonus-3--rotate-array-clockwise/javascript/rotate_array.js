1cae7631a94fcb7fc4e1c2dce127551e31e6fa47
jrog20 <jaimerogstad@gmail.com>
2021-11-25 22:38:54 -0500

    Add function

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
@@ -1,9 +1,11 @@
 function rotateArray(arr, k) {
-  // type your code here
+  const rotations = k % arr.length;
+  const removed = arr.splice(arr.length - rotations, rotations);
+
+  return removed.concat(arr);
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: [4, 1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3, 4], 1));
 
@@ -20,5 +22,32 @@ if (require.main === module) {
 
 module.exports = rotateArray;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// The problem: Rotate Array Clockwise
+
+// Given an input Array, rotate `k` units clockwise, 
+// i.e. shift the values rightward `k` units.
+
+// `k` is an Integer >= 0.
+
+// ```
+// Input: [1, 2, 3, 4], 1
+// Output: [4, 1, 2, 3]
+
+// Input: [1, 2, 3], 2
+// Output: [2, 3, 1]
+
+// Input: [1, 2, 3], 3
+// Output: [1, 2, 3]
+// ```
+
+// explanation
+//  We can solve this problem by figuring out how many elements to remove from the end
+//  of the array, and then adding those removed elements to the front of the array. 
+//  We need to account for when k is the same or larger than the array's length.
+//  We can do this by getting the remainder of k / the array length.
+
+// pseudocode
+//  initialize rotations to store the remainder of k / array length
+//  remove rotations number elements from end of array
+//  return removed elements + remaining elements
+
