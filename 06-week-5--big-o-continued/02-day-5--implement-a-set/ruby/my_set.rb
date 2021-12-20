f80bac2f3fddb18c0c36436c686786c269234281
Jaime Rogstad <jaimerogstad@gmail.com>
2021-12-19 20:01:36 -0500

    Add methods

--- 06-week-5--big-o-continued/02-day-5--implement-a-set/ruby/my_set.rb
+++ 06-week-5--big-o-continued/02-day-5--implement-a-set/ruby/my_set.rb
@@ -1,37 +1,61 @@
 class MySet 
   attr_reader :data
-  # throw an error if called with anything other than string, array or nothing
-  # if an iterable is provided only its unique values should be in data
-  # strings and arrays will need to be broken down by their elements/characters
+  
   def initialize(iterable = nil)
+    # throw an error if called with anything other than string, array or nothing
+    raise "You must initiate with and empty set, a string, or an array" unless 
+      iterable.nil? || iterable.kind_of?(String) || iterable.kind_of?(Array)
+    
     @data = {}
+    
+    # if an iterable is provided only its unique values should be in data
+    # strings and arrays will need to be broken down by their elements/characters
+    unless iterable.nil?
+      items = iterable.kind_of?(String) ? iterable.split("") : iterable
+      items.each { |item| @data[item] = true }
+    end
   end
 
   # return number of elements in MySet
   def size
+    entries.length
   end
 
   # add an item to MySet as is
   # return the MySet instance
   def add(item)
+    @data[item] = true
+    self
   end
 
   # delete an item from MySet
   # return true if successful otherwise false
   def delete(item)
+    !!@data.delete(item)
   end
 
   # return true if in MySet, otherwise false
   def has(item)
+    !!@data[item]
   end
 
   # return data as an array
   def entries
+    @data.keys
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  # Don't forget to add your own!
+  mySet = MySet.new('peri')
+  puts "Created with string peri: #{mySet.data}"
+  puts "Add 'winkle': #{mySet.add('winkle')}"
+  puts mySet.data
+  puts mySet.add('p')
+  puts mySet.data
+  puts mySet.add('s')
+  puts mySet.data
+  puts mySet.entries
+  puts mySet.size
 end
 
 
