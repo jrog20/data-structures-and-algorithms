fe1b9a821bec60f566375e0a04686e984510cb81
Jaime Rogstad <jaimerogstad@gmail.com>
2022-01-01 16:47:00 -0500

    Add method and tests

--- 07-week-6--foundational-data-structures/03-bonus-algorithm--recursive-string-reverse/javascript/reverse_string.js
+++ 07-week-6--foundational-data-structures/03-bonus-algorithm--recursive-string-reverse/javascript/reverse_string.js
@@ -1,9 +1,11 @@
 function reverseString(str) {
-  // type your code here
+  if (str.length < 2) {
+    return str;
+  } 
+  return reverseString(str.slice(1)) + str[0];
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString('ih'));
 
@@ -11,9 +13,23 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString('catbaby'));
+
+  console.log("");
+
+  console.log("Expecting: 'empty string'");
+  console.log("=>", reverseString(''));
+
+  console.log("");
+
+  console.log("Expecting: 'esor'");
+  console.log("=>", reverseString('rose'));
 }
 
 module.exports = reverseString;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// pseudocode
+// return string if length is 0 or 1
+// return reverse_string(string - 0th character) + 0th character
+
+// written explanation of solution
+// Base case => we want to return if the string is empty or length == 1.
